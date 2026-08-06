5e134f4b0741ecbf90ce6a65f1b8d1de9d963d42
Alkamashaikh <mohammed.shaikh25@mmit.edu.in>
2026-08-06 12:26:34 +0530

    add this file

---
+++ cc.py
@@ -0,0 +1,12 @@
+a=(str(input("Enter your subjects:")))
+if a == "maths" and "programming":
+ print("Suggested career path: Computer Engineering")
+elif a == "maths" and "circuits":
+ print("Suggested career path: Electronic Engineering")
+elif a == "biology" and "chemistry":
+ print("Suggested career path: Biotechnology")
+elif a == "maths" and "physics":
+ print("Suggested career path: Mechanical Engineering")
+else:
+ print("Enter valid input")
+ 
